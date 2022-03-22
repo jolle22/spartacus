7aecc29654a59cb12a93c39f1e67c9d49876d6d7
Radhep Saba <radhep.saba@gmail.com>
2022-03-22 14:26:24 -0400

    include cms data

--- projects/storefrontlib/cms-components/content/pdf/pdf.component.ts
+++ projects/storefrontlib/cms-components/content/pdf/pdf.component.ts
@@ -1,11 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CmsPDFComponent } from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
 
 @Component({
   selector: 'cx-pdf',
   templateUrl: './pdf.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PDFComponent implements OnInit {
-  constructor() {}
+
+  title: string | undefined;
+  testFunc(data: CmsPDFComponent) {
+    console.log(data);
+    this.title = data.title;
+  }
+
+  data$: Observable<CmsPDFComponent> = this.component.data$.pipe(
+    tap((data) => {
+      console.log(data);
+      this.testFunc(data);
+    })
+  );
+  constructor(
+    protected component: CmsComponentData<CmsPDFComponent>
+  ) {}
 
   ngOnInit(): void {}
 }
